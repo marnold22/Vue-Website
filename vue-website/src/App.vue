3846bc02747bd417ba598668573b0f488d8aae79
marnold22 <minimearnold@gmail.com>
2019-05-26 13:41:26 -0700

    Created Navbar and added FontAwesome to project

--- vue-website/src/App.vue
+++ vue-website/src/App.vue
@@ -1,6 +1,39 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+
+    <div class="navbar has-shadow">
+      <div class="container">
+
+        <div class="navbar-brand">
+          <a class="navbar-item">MyCompany</a>
+        </div>
+
+        <span class="navbar-toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+
+        <div class="navbar-end navbar-menu">
+          <router-link to="/" class="navbar-item r-item">Home</router-link>
+          <router-link to="/faq" class="navbar-item r-item">About</router-link>
+          <router-link to="/faq" class="navbar-item r-item">Contact</router-link>
+          <router-link to="/faq" class="navbar-item r-item">FAQ</router-link>
+
+          <div class="navbar-item">
+            <p class="control">
+              <a class="button is-primary is-outlined">
+                <span class="icon">
+                  <i class="fa fa-download"></i>
+                </span>
+                <span>Join Now</span>
+              </a>
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -14,14 +47,5 @@ export default {
 <style lang="sass">
 @import '../node_modules/bulma/bulma.sass'
 @import 'mq'
-
-
-#app
-  font-family: 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-  text-align: center
-  color: #2c3e50
-  margin-top: 60px
 
 </style>
