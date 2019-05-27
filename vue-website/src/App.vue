20e11d9eeb6f75bddd2b260dd9caf85b6489fec1
marnold22 <minimearnold@gmail.com>
2019-05-27 14:52:49 -0700

    Revamped navbar
    Updated bulma css classes
    Added isActive function for hamburger menu
    Custom sass

--- vue-website/src/App.vue
+++ vue-website/src/App.vue
@@ -1,50 +1,76 @@
 <template>
   <div id="app">
 
-    <nav class="navbar has-shadow">
-      <div class="container">
+    <nav class="navbar">
+      <div class="navbar-brand">
+        <a class="navbar-item">MyCompany</a>
 
-        <div class="navbar-brand">
-          <a class="navbar-item">MyCompany</a>
-        </div>
-
-        <span class="navbar-toggle">
+        <a class="navbar-burger burger" v-on:click="toggleNav" v-bind:class="{'is-active': isActive}">
           <span></span>
           <span></span>
           <span></span>
-        </span>
+        </a>
+      </div>
 
-        <div class="navbar-end navbar-menu">
+      <div class="navbar-menu" v-bind:class="{'is-active': isActive}">
+        <div class="navbar-end">
           <router-link to="/" class="navbar-item r-item">Home</router-link>
           <router-link to="/faq" class="navbar-item r-item">About</router-link>
           <router-link to="/faq" class="navbar-item r-item">Contact</router-link>
           <router-link to="/faq" class="navbar-item r-item">FAQ</router-link>
-
           <div class="navbar-item">
-            <p class="control">
-              <a class="button is-primary is-outlined">
-                <span class="icon">
-                  <i class="fa fa-download"></i>
-                </span>
-                <span>Join Now</span>
-              </a>
-            </p>
+            <a class="button is-primary is-outlined">
+              <span class="icon">
+                <i class="fa fa-download"></i>
+              </span>
+              <span>Join Now</span>
+            </a>
           </div>
         </div>
       </div>
     </nav>
+
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data: function() {
+    return {
+      isActive: false
+    }
+  },
+  methods: {
+    toggleNav: function() {
+      this.isActive = !this.isActive;
+    }
+  }
 }
 </script>
 
 <style lang="sass">
 @import '../node_modules/bulma/bulma.sass'
 @import 'mq'
+
+.navbar
+  background-color: #383838
+  a:hover
+    color: gray
+    background-color: #383838
+
+.r-item
+  color: #C1C1C1
+  padding: 0.5rem 1.75rem
+  +mobile
+    color: gray
+    &:hover
+      background-color: #f1f1f1
+
+
+.navbar-brand .navbar-item
+  color: #C1C1C1
+  font-weight: bold
 
 </style>
